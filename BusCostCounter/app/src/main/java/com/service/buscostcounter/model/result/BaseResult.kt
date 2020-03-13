a7aa2b24229e72f0761eeb31e1d0becf8648c517
Sofi <sofi.harutyunyan@develandoo.am>
2020-03-13 18:41:28 +0400

    added new parent class for results to make polymorphism work

---
+++ BusCostCounter/app/src/main/java/com/service/buscostcounter/model/result/BaseResult.kt
@@ -0,0 +1,8 @@
+package com.service.buscostcounter.model.result
+
+import com.service.buscostcounter.model.bus.Bus
+
+open class BaseResult {
+
+    var bus: Bus? = null
+}
--- BusCostCounter/app/src/main/java/com/service/buscostcounter/model/result/Fastest.kt
+++ BusCostCounter/app/src/main/java/com/service/buscostcounter/model/result/Fastest.kt
@@ -1,8 +1,3 @@
 package com.service.buscostcounter.model.result
 
-import com.service.buscostcounter.model.bus.Bus
-
-class Fastest {
-
-    var bus: Bus ?= null
-}
+class Fastest : BaseResult()
